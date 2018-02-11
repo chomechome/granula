555358de1b152a65f8419b38a90186e938bf5165
Vladislav Blinov <cunningplan@yandex.ru>
2018-02-11 22:56:08 +0500

    Refresh the version

--- granula/__init__.py
+++ granula/__init__.py
@@ -3,4 +3,4 @@ from .dsl import DSLResolver, DSLFinder
 from .pattern import Any, All, Inverse, Environment, Extension, Wildcard, Regex
 from .serializer import YAMLSerializer, JSONSerializer
 
-__version__ = '2018.02.4'
+__version__ = '2018.02.1'
